566a4c22ef0d4c82a8b056437b70ca1d9ef284da
jtandria <jt.andriamihamina@gmail.com>
2023-11-05 12:40:41 +0100

    Improve readability on fish install script

--- src/fish/install.sh
+++ src/fish/install.sh
@@ -9,8 +9,12 @@ then
     apt-get install -y curl gnupg
 
     # Add package to local repo
-    echo "deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/${OS_ID}_${OS_RELEASE}/ /" | tee /etc/apt/sources.list.d/shells:fish:release:3.list
-    curl -fsSL "https://download.opensuse.org/repositories/shells:fish:release:3/${OS_ID}_${OS_RELEASE}/Release.key" | gpg --dearmor | tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
+    echo "deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/${OS_ID}_${OS_RELEASE}/ /" \
+        | tee /etc/apt/sources.list.d/shells:fish:release:3.list
+
+    curl -fsSL "https://download.opensuse.org/repositories/shells:fish:release:3/${OS_ID}_${OS_RELEASE}/Release.key" \
+        | gpg --dearmor \
+        | tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
 fi
 
 # Install app
